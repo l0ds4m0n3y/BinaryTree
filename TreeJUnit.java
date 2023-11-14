cf6f3f1a674b26d16851da40aaf65e993bc4a2c9
l0ds4m0n3y <103957548+l0ds4m0n3y@users.noreply.github.com>
2023-11-14 14:57:31 -0800

    Update TreeJUnit.java

--- TreeJUnit.java
+++ TreeJUnit.java
@@ -23,4 +23,40 @@ public class TreeJUnit {
         myTree.add(9999);
         assertEquals(5, myTree.size());
     }
+
+    @Test
+    void testNumInterior(){
+        myTree.add(10);
+        myTree.add(5);
+        myTree.add(13);
+        myTree.add(3);
+        myTree.add(7);
+        myTree.add(11);
+        myTree.add(15);
+        assertEquals(4, myTree.leaves());
+
+        myTree.add(4);
+        assertEquals(4, myTree.leaves());
+
+        myTree.add(2);
+        assertEquals(5, myTree.leaves());
+    }
+
+    @Test
+    void testInterior(){
+        myTree.add(10);
+        myTree.add(5);
+        myTree.add(13);
+        myTree.add(3);
+        myTree.add(7);
+        myTree.add(11);
+        myTree.add(15);
+        assertEquals(3, myTree.interiorNodes());
+
+        myTree.add(4);
+        // assertEquals(4, myTree.interiorNodes());
+
+        myTree.add(2);
+        assertEquals(4, myTree.interiorNodes());
+    }
 }
